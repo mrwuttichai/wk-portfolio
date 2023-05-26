3f4485f217a27bccb95390bd0461547f8ce27abd
wuttichai <mr.wuttichai.ktsm@gmail.com>
2023-05-26 15:29:37 +0700

    add-work-experience

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -4,13 +4,13 @@ export default function AboutMeSection() {
   return (
     <div className="p-8">
       <div>
-          <h1 className="uppercase">About me</h1>
+          <h1 className="uppercase ">About me</h1>
       </div>
       <div className="flex items-center gap-x-10">
         <div className="flex flex-col justify-center items-center w-1/2">
           <div>
-            <h1 className="text-2xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h1>
-            <p className="text-2xl pt-10 pb-20">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <h1 className="text-2xl"></h1>
+            <p className="text-2xl pt-10 pb-20"> Dynamic and versatile software developer with a background in law, agriculture, and business. Skilled in front-end (HTML, CSS, Bootstrap, JavaScript, React JS), back-end (Node JS, Express JS), and database development (MongoDB, SQL). Collaborative team player experienced in agile-scrum methodologies. Strong communicator with problem-solving and strategic thinking abilities. Passionate about agritech, business and AI. Dedicated to creating effective digital solutions.</p>
             <button className="bg-black text-white px-4 py-2 rounded-sm transition-all hover:bg-slate-100 hover:text-black">RESUME</button>
           </div>
         </div>
